d7bbce9f6c5dab58eeabe04140cb7875868bf549
audgnssweet <audgnssweet@naver.com>
2021-08-28 13:56:44 +0900

    데이터 수정

--- src/main/kotlin/com/corona/backend/controller/TestController.kt
+++ src/main/kotlin/com/corona/backend/controller/TestController.kt
@@ -1,5 +1,6 @@
 package com.corona.backend.controller
 
+import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestController {
 
+    @ApiOperation("에러봇 테스트")
     @PostMapping("/test")
     fun test(@RequestBody dto: Dto) {
         throw IllegalArgumentException("test value is null")
