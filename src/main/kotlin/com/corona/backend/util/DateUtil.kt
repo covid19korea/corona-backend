52f391721034e7c3f7a3cdb63aacfddb1b42e7e9
audgnssweet <audgnssweet@naver.com>
2021-08-28 14:08:28 +0900

    시차 적용

--- src/main/kotlin/com/corona/backend/util/DateUtil.kt
+++ src/main/kotlin/com/corona/backend/util/DateUtil.kt
@@ -1,17 +1,19 @@
 package com.corona.backend.util
 
 import java.time.LocalDate
-import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 object DateUtil {
 
     fun getDate(): LocalDate {
-        return if (isYesterday(LocalDateTime.now())) {
-            LocalDate.now().minusDays(1)
-        } else LocalDate.now()
+        val nowSeoul = ZonedDateTime.now(ZoneId.of("Asia/Seoul"))
+        return if (isYesterday(nowSeoul)) {
+            ZonedDateTime.now(ZoneId.of("Asia/Seoul")).minusDays(1).toLocalDate()
+        } else ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalDate()
     }
 
-    private fun isYesterday(time: LocalDateTime): Boolean {
+    private fun isYesterday(time: ZonedDateTime): Boolean {
         return (time.hour < 9) || (time.hour == 9 && time.minute < 30)
     }
 
