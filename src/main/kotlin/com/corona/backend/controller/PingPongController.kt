fe6bb20aa4e956bc4c3cd586e04a41ba8a08334d
audgnssweet <audgnssweet@naver.com>
2021-11-12 10:29:24 +0900

    feat: 현지시간 타이머

--- src/main/kotlin/com/corona/backend/controller/PingPongController.kt
+++ src/main/kotlin/com/corona/backend/controller/PingPongController.kt
@@ -2,6 +2,7 @@ package com.corona.backend.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 class PingPongController {
@@ -9,5 +10,10 @@ class PingPongController {
     @GetMapping("/ping")
     fun ping(): String {
         return "pong"
+    }
+
+    @GetMapping("/time")
+    fun time(): String {
+        return LocalDateTime.now().toString()
     }
 }
