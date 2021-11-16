16f94edd86820057878b111a6e00573920ac807e
audgnssweet <audgnssweet@naver.com>
2021-11-16 11:39:12 +0900

    chore: ktlint 적용

--- src/main/kotlin/com/corona/backend/controller/PingPongController.kt
+++ src/main/kotlin/com/corona/backend/controller/PingPongController.kt
@@ -2,7 +2,6 @@ package com.corona.backend.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import java.lang.IllegalArgumentException
 import java.time.LocalDateTime
 
 @RestController
