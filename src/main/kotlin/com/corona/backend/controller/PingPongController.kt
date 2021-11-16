35714808eaafc21137f01b8cc47bf576c3386d12
audgnssweet <audgnssweet@naver.com>
2021-11-16 11:38:37 +0900

    fix: scheduler에서 error push 되지 않던 문제 해결

--- src/main/kotlin/com/corona/backend/controller/PingPongController.kt
+++ src/main/kotlin/com/corona/backend/controller/PingPongController.kt
@@ -2,6 +2,7 @@ package com.corona.backend.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.IllegalArgumentException
 import java.time.LocalDateTime
 
 @RestController
--- src/main/kotlin/com/corona/backend/scheduler/CacheScheduler.kt
+++ src/main/kotlin/com/corona/backend/scheduler/CacheScheduler.kt
@@ -1,8 +1,13 @@
 package com.corona.backend.scheduler
 
 import com.corona.backend.cacher.DataCacher
+import com.corona.backend.errorbot.configs.LogConfig
+import com.corona.backend.errorbot.entity.ErrorLog
 import com.corona.backend.service.PublicDataService
 import com.corona.backend.util.DateUtil
+import net.gpedro.integrations.slack.SlackApi
+import net.gpedro.integrations.slack.SlackAttachment
+import net.gpedro.integrations.slack.SlackMessage
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -10,11 +15,12 @@ import org.springframework.stereotype.Component
 class CacheScheduler(
     private val publicDataService: PublicDataService,
     private val dataCacher: DataCacher,
+    private val logConfig: LogConfig,
 ) {
-    @Scheduled(cron = "1 15 1 * * *") // 한국시간기준 오전 10시 15분
+
+    @Scheduled(cron = "0 15 1 * * *") // 한국시간기준 오전 10시 15분
     fun cache() {
         cacheTodayData()
-        throw IllegalArgumentException()
     }
 
     private fun cacheTodayData() {
@@ -22,8 +28,15 @@ class CacheScheduler(
             cacheAll()
         } catch (e: Exception) {
             handleCacheError()
-            throw e
+            pushSlack(e)
         }
+    }
+
+    private fun cacheAll() {
+        publicDataService.getInfection()
+        publicDataService.getInfectionRegion()
+        publicDataService.getInoculation()
+        publicDataService.getInoculationRegion()
     }
 
     private fun handleCacheError() {
@@ -33,10 +46,34 @@ class CacheScheduler(
         dataCacher.cache(DateUtil.getDate(), dataCacher.getInoculationRegion(DateUtil.getDate(1))!!)
     }
 
-    private fun cacheAll() {
-        publicDataService.getInfection()
-        publicDataService.getInfectionRegion()
-        publicDataService.getInoculation()
-        publicDataService.getInoculationRegion()
+    private fun pushSlack(e: Exception) {
+        val slackApi = SlackApi(logConfig.slack.webHookUrl)
+        slackApi.call(generateSlackMessage(generateSlackAttachment(ErrorLog(trace = generateStackTrace(e)))))
+    }
+
+    private fun generateSlackMessage(slackAttachment: SlackAttachment): SlackMessage {
+        return SlackMessage("").apply {
+            setChannel("#" + logConfig.slack.channel)
+            setUsername("ErrorBot")
+            setIcon(":exclamation:")
+            setAttachments(listOf(slackAttachment))
+        }
+    }
+
+    private fun generateSlackAttachment(errorLog: ErrorLog?): SlackAttachment {
+        return SlackAttachment().apply {
+            setFallback("서버 에러 발생!")
+            setColor("danger")
+            setTitle(errorLog!!.message)
+            setText(errorLog.trace)
+        }
+    }
+
+    private fun generateStackTrace(e: Exception): String {
+        val sb = StringBuilder()
+        sb.append(e.javaClass.name).append('\n')
+        e.stackTrace
+            .forEach { sb.append("${it.className}:${it.lineNumber}\n") }
+        return sb.toString()
     }
 }
