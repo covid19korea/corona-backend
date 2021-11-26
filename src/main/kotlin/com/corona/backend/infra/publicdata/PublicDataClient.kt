0b344a484c40b3d68ee9e60c8dc3b09a5fe9c5cb
audgnssweet <audgnssweet@naver.com>
2021-11-26 16:44:00 +0900

    feat: webclient timeout을 10초로 설정

--- src/main/kotlin/com/corona/backend/infra/publicdata/PublicDataClient.kt
+++ src/main/kotlin/com/corona/backend/infra/publicdata/PublicDataClient.kt
@@ -1,6 +1,9 @@
 package com.corona.backend.infra.publicdata
 
 import com.corona.backend.exception.PublicDataException
+import io.netty.channel.ChannelOption
+import io.netty.handler.timeout.ReadTimeoutHandler
+import io.netty.handler.timeout.WriteTimeoutHandler
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.stereotype.Component
@@ -9,13 +12,20 @@ import org.springframework.util.MultiValueMap
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.DefaultUriBuilderFactory
 import reactor.netty.http.client.HttpClient
+import reactor.netty.tcp.TcpClient
 import java.net.URLEncoder
 import java.time.Duration
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.MILLISECONDS
 
 @Component
 class PublicDataClient(
     @Value("\${open-api.key}") private val secretKey: String,
 ) {
+
+    private val CUSTOM_CONNECTION_TIMEOUT_MILLS = 10000
+    private val CUSTOM_READ_TIMEOUT_MILLS = 10000L
+    private val CUSTOM_WRITE_TIMEOUT_MILLS = 10000L
 
     fun getData(url: String, queryParam: MultiValueMap<String, String>?): String {
         val webClient = generateWebClient(url)
@@ -37,13 +47,25 @@ class PublicDataClient(
     }
 
     private fun generateWebClient(url: String): WebClient {
+        val tcpClient = generateTcpClient()
+
         val factory = DefaultUriBuilderFactory(url)
         factory.encodingMode = DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY
         return WebClient.builder()
-            .clientConnector(ReactorClientHttpConnector(HttpClient.create().responseTimeout(Duration.ofSeconds(10))))
+            .clientConnector(ReactorClientHttpConnector(HttpClient.from(tcpClient)))
             .uriBuilderFactory(factory)
             .baseUrl(url)
             .build()
+    }
+
+    private fun generateTcpClient(): TcpClient {
+        val tcpClient = TcpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CUSTOM_CONNECTION_TIMEOUT_MILLS)
+            .doOnConnected {
+                it.addHandlerFirst(ReadTimeoutHandler(CUSTOM_READ_TIMEOUT_MILLS, MILLISECONDS))
+                    .addHandlerLast(WriteTimeoutHandler(CUSTOM_WRITE_TIMEOUT_MILLS, MILLISECONDS))
+            }
+        return tcpClient
     }
 
     private fun setSecretKey(queryParam: MultiValueMap<String, String>?): MultiValueMap<String, String> {
